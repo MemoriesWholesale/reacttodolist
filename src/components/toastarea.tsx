3f9b44eb43c8547ba8a7a42d34b31a8a22130f48
Chandramani Ramachandran <chandramani.ramachandran@gmail.com>
2023-06-22 00:29:52 -0700

    added autohide to alerts

--- src/components/toastarea.tsx
+++ src/components/toastarea.tsx
@@ -10,7 +10,7 @@ export default function Toastarea({toasts}: Props) {
   return (
     <ToastContainer position="top-end">
         {toasts.map((t,i)=>
-            <Toast key={i} show={openToasts[i]} onClose={()=>setOpenToasts([...openToasts.slice(0,i),false,...openToasts.slice(i+1)])}>
+            <Toast key={i} show={openToasts[i]} onClose={()=>setOpenToasts([...openToasts.slice(0,i),false,...openToasts.slice(i+1)])} delay={3000} autohide>
             <Toast.Header>
               <strong className="me-auto">Alert!</strong>
             </Toast.Header>
